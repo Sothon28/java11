4c7714cb5fcacb9a538a6b7ee97d72683142dd88
Thoeurn Sothon <thoeurnsothon@mcnc.co.kr>
2022-12-15 10:37:53 +0700

    first commit

---
+++ src/main/java/com/mcnc/webadmin/WebAdminTestdbApplication.java
@@ -0,0 +1,13 @@
+package com.mcnc.webadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdminTestdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAdminTestdbApplication.class, args);
+	}
+
+}
